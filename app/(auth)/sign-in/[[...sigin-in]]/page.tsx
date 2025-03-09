0e479b7e3263a7d6e38aee67816a1106b02cf0bd
Arashdeep <arashdeep@Mac.lan>
2025-03-09 13:51:50 -0700

    test changes

--- app/(auth)/sign-in/[[...sigin-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sigin-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <main className="flex h-screen w-full items-center justify-center bg-gray-light">
+    <main className="flex h-screen w-full items-center justify-center bg-gray-light auth-main-container">
       <div className="bg-white p-8 rounded-lg shadow-lg">
         <SignIn />
       </div>
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
-    <main className="flex h-screen w-full items-center justify-center bg-gray-light">
+    <main className="flex h-screen w-full items-center justify-center bg-gray-light auth-main-container">
       <div className="bg-white p-8 rounded-lg shadow-lg">
         <SignUp />
       </div>
