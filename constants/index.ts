d7e853ba4a071665d3f9d10fe7e5078b9389e29d
Arashdeep <arashdeep@Mac.lan>
2025-03-09 16:33:41 -0700

    test changes

--- constants/index.ts
+++ constants/index.ts
@@ -1,27 +1,27 @@
 export const links = [
-    {
-        label: 'Home',
-        route: '/',
-        imgUrl:'/icons/Home.svg',
-    },
-     {
-        label: 'Upcoming',
-        route: '/upcoming',
-        imgUrl:'/icons/upcoming.svg',
-    },
-      {
-        label: 'Previous',
-        route: '/previous',
-        imgUrl:'/icons/previous.svg',
-    },
-       {
-        label: 'Recordings',
-        route: '/recordings',
-        imgUrl:'/icons/video.svg',
-    },
-        {
-        label: 'Personal Room',
-        route: '/personal-room',
-        imgUrl:'/icons/add-personal.svg',
-    },
-]
+  {
+    label: "Home",
+    route: "/",
+    imgUrl: "/icons/Home.svg",
+  },
+  {
+    label: "Upcoming",
+    route: "/upcoming",
+    imgUrl: "/icons/upcoming.svg",
+  },
+  {
+    label: "Previous",
+    route: "/previous",
+    imgUrl: "/icons/previous.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/recordings",
+    imgUrl: "/icons/recordings.svg",
+  },
+  {
+    label: "Personal Room",
+    route: "/personal-room",
+    imgUrl: "/icons/add-personal.svg",
+  },
+];
