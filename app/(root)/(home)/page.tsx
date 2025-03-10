1ba88128251ea957f8bfe81f4842bc2aa64a96dd
Arashdeep <arashdeep@Mac.lan>
2025-03-09 21:21:27 -0700

    Newer changes

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,9 +2,10 @@ import MainMeetingList from "@/components/MainMeetingList";
 
 const Home = () => {
   const now = new Date();
-  const time = now.toLocaleTimeString("en-US", {
+  const time = now.toLocaleTimeString(undefined, {
     hour: "2-digit",
     minute: "2-digit",
+    timeZoneName: "short",
   });
   const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
     now
@@ -12,7 +13,7 @@ const Home = () => {
 
   return (
     <section className="flex size-full flex-col gap-5 text-gray-dark">
-      <div className="h-[303px] w-full rounded-xl pic bg-cover bg-pink-100 p-6">
+      <div className="h-[303px] w-full rounded-xl pic bg-cover banner p-6">
         <div className="flex h-full flex-col justify-between px-6 py-8 lg:p-10">
           <div className="flex flex-col gap-2">
             <h1 className="glassmorphism max-w-[350px] rounded py-3  text-center text-7xl font-bold">
